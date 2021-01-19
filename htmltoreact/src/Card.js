77fd2e18d40e9777d8901abfa9f08a3dd57ee0e2
tyagiashish007 <tyagiashishofficial@gmail.com>
2021-01-20 01:31:51 +0530

    card section added

---
+++ htmltoreact/src/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = ({title="Basketball",button="MY BUTTON", text="mytext"}) =>{
+    return(
+        <div className="card" style={{width: "18rem"}}>
+              <img
+                src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">
+                  {text}
+                </p>
+                <a href="#" className="btn btn-success">{button}</a>
+              </div>
+            </div>
+    )
+}
+
+export default Card;
--- htmltoreact/src/Cardsection.js
+++ htmltoreact/src/Cardsection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Card from './Card'
+
 const Cardsection = () =>{
     return(
 
@@ -10,22 +12,15 @@ const Cardsection = () =>{
         </h2>
         <div className="row">
           <div className="col-4">
-            <div className="card" style={{width: "18rem"}}>
-              <img
-                src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-success">Go somewhere</a>
-              </div>
-            </div>
+
+          <Card title={"jamesbond"} text={"hello"} button={"click me"}/>
+
+          
+
+          
+            
           </div>
+          
           <div className="col-4">
             <div className="card" style={{width: "18rem"}}>
               <img
